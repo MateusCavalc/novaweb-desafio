07fd096fd80d4eae738bff0fc05450e49b922550
Mateus Cavalcanti <mat.fcavalcanti@gmail.com>
2022-06-22 16:59:40 -0300

    adiciona rotas de select e insert

--- Postgres.py
+++ Postgres.py
@@ -1,17 +1,21 @@
+import logging
 import urllib.parse as up
 import psycopg2
+from psycopg2.extras import LoggingConnection
 
 POSTGRES_URL = "postgres://fywwmpwc:tch3IBBOrFOQFdUs6QPd_UosRQjqPByT@kesavan.db.elephantsql.com/fywwmpwc"
 
 class Postgres:
     def __init__(self):
         up.uses_netloc.append("postgres")
-        self.url = up.urlparse(POSTGRES_URL)
+        url = up.urlparse(POSTGRES_URL)
+        self.db_settings = {
+            "user": url.username,
+            "password": url.password,
+            "host": url.hostname,
+            "port": url.port,
+            "database": url.path[1:],
+        }
 
     def connectToDataBase(self):
-        return psycopg2.connect(database=self.url.path[1:],
-            user=self.url.username,
-            password=self.url.password,
-            host=self.url.hostname,
-            port=self.url.port
-        )
+        return psycopg2.connect(**self.db_settings)
--- Server.py
+++ Server.py
@@ -5,7 +5,18 @@ import os
 from Postgres import Postgres
 
 DATABASE_INSTANCE = Postgres()
-DEFAULT_SCHEMA = 'novaweb-desafio'
+
+def Get_ContatoID_by_name(cur, nome):
+    try:
+        query = 'SELECT contato_id ' + \
+                'FROM contato ' + \
+                'WHERE nome=\'' + nome + '\''
+
+        cur.execute(query)
+        data_row = cur.fetchone()
+        return data_row[0]
+    except Exception as e:
+        raise e
 
 class MyHTTPRequestHandler(BaseHTTPRequestHandler):
 
@@ -14,57 +25,110 @@ class MyHTTPRequestHandler(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'application/json')
         self.end_headers()
 
+    def _no_route(self):
+        self.send_response(404)
+        self.end_headers()
+        html = '<h1>Não existe rota \'' + self.path + '\' no momento.<h1>'
+        self.wfile.write(html.encode())
+
     def do_HEAD(self):
         self._set_headers()
 
-    #handle POST command
-    def do_POST(self):
-        length = int(self.headers.get('content-length'))
-        request_body = json.loads(self.rfile.read(length))
-
-        conn = DATABASE_INSTANCE.connectToDataBase()
-
-        cur = conn.cursor()
-        cur.execute('SET SEARCH_PATH TO \'' + DEFAULT_SCHEMA + '\'')
-        cur.execute(request_body['query'])
-        conn.commit()
-
-        print(cur.rowcount, "Record inserted successfully")
-
-        cur.close()
-        conn.close()
-
-        responsePayload = {'status': 'POST great'}
-
-        # Envia informações da assinatura para o cliente
-        self._set_headers()
-        self.wfile.write(json.dumps(responsePayload).encode())
-
-    #handle GET command
+    #handle GET method
     def do_GET(self):
         length = int(self.headers.get('content-length'))
         request_body = json.loads(self.rfile.read(length))
 
-        conn = DATABASE_INSTANCE.connectToDataBase()
+        curr_schema = request_body['schema']
 
-        cur = conn.cursor()
-        cur.execute('SET SEARCH_PATH TO \'' + DEFAULT_SCHEMA + '\'')
-        cur.execute(request_body['query'])
-        result = cur.fetchall()
-        
-        for row in result:
-            print(row[1] + ' / ' + row[2])
+        try:
+            conn = DATABASE_INSTANCE.connectToDataBase()
+            cur = conn.cursor()
+            cur.execute('SET SEARCH_PATH TO \'' + curr_schema + '\'')
+            
+            if self.path == '/contatos':
+                query = 'SELECT * ' + \
+                        'FROM contato'
+            elif self.path == '/telefones':
+                query = 'SELECT telefone.telefone_id, contato.nome, telefone.telefone ' + \
+                        'FROM contato ' + \
+                        'INNER JOIN telefone ON contato.contato_id=telefone.contato_id'
+            else:
+                self._no_route()
+                return
 
-        cur.close()
-        conn.close()
+            cur.execute(query)
+            columns = cur.description
 
-        responsePayload = {'status': 'GET great'}
+            data_array = []
+            for row in cur.fetchall():
+                data = {}
+                for i, row_data in enumerate(row):
+                    data[columns[i][0]] = row_data
+
+                data_array.append(data)
+
+            responsePayload = {'status': 'success'}
+            responsePayload['data'] = data_array
+
+        except Exception as e:
+            print('> Error:', e)
+            responsePayload = {'status': 'failed', 'error': str(e)}
+        finally:
+            cur.close()
+            conn.close()
 
         # Envia informações da assinatura para o cliente
         self._set_headers()
         self.wfile.write(json.dumps(responsePayload).encode())
 
-    #handle PUT command
+    #handle POST method
+    def do_POST(self):
+        length = int(self.headers.get('content-length'))
+        request_body = json.loads(self.rfile.read(length))
+
+        curr_schema = request_body['schema']
+        responsePayload = {}
+
+        try:
+            conn = DATABASE_INSTANCE.connectToDataBase()
+            cur = conn.cursor()
+            cur.execute('SET SEARCH_PATH TO \'' + curr_schema + '\'')
+
+            if self.path == '/contato':
+                user_data = request_body['infos']
+                query = 'INSERT INTO contato (nome, email) ' + \
+                        'VALUES (\'{}\', \'{}\')' \
+                        .format(user_data['nome'], user_data['email'])
+            elif self.path == '/telefone':
+                user_data = request_body['infos']
+
+                contato_id = Get_ContatoID_by_name(cur, user_data['nome']);
+
+                query = 'INSERT INTO telefone (contato_id, telefone) ' + \
+                        'VALUES (\'{}\', \'{}\')' \
+                        .format(contato_id, user_data['telefone'])
+
+            else:
+                self._no_route()
+                return
+            
+            cur.execute(query)
+            conn.commit()
+            responsePayload = {'status': 'success'}
+
+        except Exception as e:
+            print('> Error:', e)
+            responsePayload = {'status': 'failed', 'error': str(e)}
+        finally:
+            cur.close()
+            conn.close()
+
+        # Envia informações da assinatura para o cliente
+        self._set_headers()
+        self.wfile.write(json.dumps(responsePayload).encode())
+
+    #handle PUT method
     def do_PUT(self):
         length = int(self.headers.get('content-length'))
         request_body = json.loads(self.rfile.read(length)) 
@@ -75,7 +139,7 @@ class MyHTTPRequestHandler(BaseHTTPRequestHandler):
         self._set_headers()
         self.wfile.write(json.dumps(responsePayload).encode())
 
-    #handle DELETE command
+    #handle DELETE method
     def do_DELETE(self):
         length = int(self.headers.get('content-length'))
         request_body = json.loads(self.rfile.read(length)) 
